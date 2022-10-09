bab5379a5244f13914520d51a9c3e9de9a352737
Zombiedenden <42314738+Zombiedenden@users.noreply.github.com>
2022-10-09 16:21:14 +1100

    Create OAuth2 credentials and save as token.json

---
+++ basic_tester.py
@@ -0,0 +1,33 @@
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+CLIENT_SECRET_FILE = 'client_secret.json'
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/bigquery',
+    # 'https://mail.google.com/'
+    ]
+
+
+def main():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if not creds or not creds.valid():
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            creds = flow.run_local_server()
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+if __name__ == '__main__':
+    main()
+
+
