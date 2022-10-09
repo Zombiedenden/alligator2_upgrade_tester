c7a0723d6fa7d63fbc1f7c0443b0ab911ae1c4f1
Zombiedenden <42314738+Zombiedenden@users.noreply.github.com>
2022-10-09 17:21:33 +1100

    add CLI args and a bigquery test
    Added command line args to dynamically push a gcp project id
    Added at BQ test pulling data from a public dataset (covid19_open_data)

--- basic_tester.py
+++ basic_tester.py
@@ -1,4 +1,7 @@
+import argparse
 import os
+import sys
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -13,8 +16,15 @@ SCOPES = [
 ]
 
 
-def main():
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--project_id", required=True, type=str, help="a Google Cloud Project ID")
+    args = parser.parse_args()
+
+    project_id = args.project_id
     creds = None
+    sys.argv.clear()
+
     # look for existing Oauth token
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
@@ -24,18 +34,35 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CLIENT_SECRET_FILE, SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
             # creates local server for authentication and captures the response
             creds = flow.run_local_server()
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
     # build a gmail service object
-    service_gmail = build("gmail", "v1", credentials=creds)
+    gmail_service = build("gmail", "v1", credentials=creds)
     # simple test to make sure the service is working
-    print(dir(service_gmail))
+    print(dir(gmail_service))
+
+    # simple test to make sure the service is working
+    bq_service = build("bigquery", "v2", credentials=creds)
+    query = {
+        "query": """
+        SELECT
+          date,
+          place_id
+        FROM
+          [bigquery-public-data:covid19_open_data.covid19_open_data]
+        LIMIT 3
+      """
+    }
+
+    response_json = bq_service.jobs().query(body=query, projectId=project_id).execute()
+    rows = response_json.get("rows") or []
+    for row in rows:
+        print(rows)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
